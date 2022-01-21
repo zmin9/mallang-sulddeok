f79db5530fc4bb1deb5c40ce7d5886f79a6e6559
zmin9 <o3o0903@gmail.com>
2022-01-22 01:11:56 +0900

    [fix] fix result bug ,etc
    - 폴더 이동 후 npm start 안 먹던 오류 해결
    - 계속 같은 결과만 나오던 현상 수정
    - Main컨포넌트에 페이지 수 추가

--- mallangsulddeok/src/Main.js
+++ mallangsulddeok/src/Main.js
@@ -4,27 +4,40 @@ import testScript from './question.json'
 const Main = ({setPage, userValue, setUserValue}) => {
     let Qnum = 11;
     let timer;
+    let userAnswer = -1;
     const selectedStyle = {
         background:"lightgray",
         cursor:"default"
     }
 
     const [index, setIndex] = useState(0);
-    const [userAnswer, setUserAnswer] = useState(-1);
+    const [selected, setSelected] = useState(-1);
     
-
     const onClickAnswer = (e) => {
-        (e.target.className === 'Answer1' ? setUserAnswer(0) : setUserAnswer(1))
+        
+        if(e.target.className === 'Answer1'){
+            userAnswer = 0;
+            setSelected(0);
+        }
+        else {
+            userAnswer = 1;
+            setSelected(1);
+        }
+
         const temp = [...userValue];
-        temp[testScript.test[index].type] += testScript.test[index]["score"][0];
+        temp[testScript.test[index].type] += testScript.test[index]["score"][userAnswer];
+        /*
+        console.log(userAnswer);
+        console.log(testScript.test[index]["score"][userAnswer]);
         console.log(temp);
+        */
 
         setUserValue(temp);
         timer = setTimeout(()=>{
             // 질문 넘기기
             (index < Qnum ? setIndex(index + 1) : setPage(2))
             // 선택된 값 변경
-            setUserAnswer(-1);
+            setSelected(-1);
         }, 500); // 0.5초 뒤에 함수 실행
     }
 
@@ -40,19 +53,13 @@ const Main = ({setPage, userValue, setUserValue}) => {
                 <div className="Question">
                     {testScript.test[index].question}
                 </div>
-                <div className='Answer1' onClick={userAnswer === -1 ? onClickAnswer : null} style={ userAnswer === 0 ? selectedStyle : null }>
+                <div className='Answer1' onClick={userAnswer === -1 ? onClickAnswer : null} style={ selected === 0 ? selectedStyle : null }>
                     {testScript.test[index].answer[0]}
                 </div>
-                <div className='Answer2' onClick={userAnswer === -1 ? onClickAnswer : null} style={ userAnswer === 1 ? selectedStyle : null }>
+                <div className='Answer2' onClick={userAnswer === -1 ? onClickAnswer : null} style={ selected === 1 ? selectedStyle : null }>
                     {testScript.test[index].answer[1]}
                 </div>
-                {/* 
-                <div 
-                    onClick={()=>{controlTest();setUserAnswer(-1);}}
-                    style={{cursor:"pointer"}}>
-                    다음
-                </div>
-                */}
+                <div style={{textAlign:"center",fontWeight:"bold"}}>{index + 1}/{Qnum + 1}<br/></div>
             </div>
         </div>
     )
