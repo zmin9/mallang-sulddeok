9211142d8fa6c1e46da23a68b042e5b77df28dd5
zmin9 <o3o0903@gmail.com>
2022-03-14 11:02:13 +0900

    [fix] 카카오톡 공유하기 시 썸네일 안 뜨는 문제

--- mallangsulddeok/src/SocialShare.js
+++ mallangsulddeok/src/SocialShare.js
@@ -87,53 +87,79 @@ const BackBtn = styled.div`
 
 function SocialShare({setPopupOpen}) {
     // window 객체에서 현재 url 가져오기
-      const currentUrl = window.location.href;
-	  //kakao SDK import 하기
-	  const status = useScript("https://developers.kakao.com/sdk/js/kakao.js");
+	const currentUrl = window.location.href;
+	//kakao SDK import 하기
+	const status = useScript("https://developers.kakao.com/sdk/js/kakao.js");
 
-	  // kakao sdk 초기화하기
-	  // status가 변경될 때마다 실행되며, status가 ready일 때 초기화를 시도합니다.
-	  useEffect(() => {
-		  if (status === "ready" && window.Kakao) {
-			  // 중복 initialization 방지
-			  if (!window.Kakao.isInitialized()) {
-				  // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
-				  window.Kakao.init("687249173015ebd36dbbab09efdc58b6");
-			  }
+	// kakao sdk 초기화하기
+	// status가 변경될 때마다 실행되며, status가 ready일 때 초기화를 시도합니다.
+	useEffect(() => {
+	  if (status === "ready" && window.Kakao) {
+		  // 중복 initialization 방지
+		  if (!window.Kakao.isInitialized()) {
+			  // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
+			  window.Kakao.init("687249173015ebd36dbbab09efdc58b6");
 		  }
-	  }, [status]);	
-	  
-	  const handleKakaoButton = () => {
-        window.Kakao.Link.sendScrap({
-            requestUrl: currentUrl,
-        });
-    };
+	  }
+	}, [status]);
+
+	const handleKakaoButton = () => {
+		window.Kakao.Link.sendDefault({
+			objectType: 'feed',
+			content: {
+				title: '말랑술떡이 알려주는 술 MBTI',
+				description: '당신의 술을 확인하세요!',
+				imageUrl:
+					'/home_image.jpg',
+				link: {
+					mobileWebUrl: currentUrl,
+					webUrl: currentUrl,
+				},
+			},
+			buttons: [
+				{
+					title: '결과 보기',
+					link: {
+						mobileWebUrl: currentUrl,
+						webUrl: currentUrl,
+					},
+				},
+				{
+					title: '나도 검사하기',
+					link: {
+						mobileWebUrl: 'https://alcohol-mbti.netlify.app/',
+						webUrl: 'https://alcohol-mbti.netlify.app/',
+					},
+				},
+			],
+		});
+	};
 
 
-      return (
-		  <Popup>
-				<FlexContainer>
-					<h2 style={{color:'#1f170b'}}>공유하기</h2>
-					<GridContainer>
-						<FacebookShareButton url={currentUrl}>
-							<FacebookIcon size={48} round={true} borderRadius={24}></FacebookIcon>
-						</FacebookShareButton>
-						<TwitterShareButton url={currentUrl}>
-							<TwitterIcon size={48} round={true} borderRadius={24}></TwitterIcon>
-						</TwitterShareButton>
-						<CopyToClipboard text={currentUrl}>
-							<URLShareButton>URL</URLShareButton>
-						</CopyToClipboard>
-						<KakaoShareButton onClick={handleKakaoButton}>
-							<KakaoIcon src={kakaoLogo}></KakaoIcon>
-						</KakaoShareButton>
-					</GridContainer>
-					<BackBtn onClick={()=>(setPopupOpen(false))}>닫기</BackBtn>
-				</FlexContainer>
+	return (
+		<Popup>
+			<FlexContainer>
+				<h2 style={{color:'#1f170b'}}>공유하기</h2>
+				<GridContainer>
+					<FacebookShareButton url={currentUrl}>
+						<FacebookIcon size={48} round={true} borderRadius={24}></FacebookIcon>
+					</FacebookShareButton>
+					<TwitterShareButton url={currentUrl}>
+						<TwitterIcon size={48} round={true} borderRadius={24}></TwitterIcon>
+					</TwitterShareButton>
+					<CopyToClipboard text={currentUrl}>
+						<URLShareButton>URL</URLShareButton>
+					</CopyToClipboard>
+					<KakaoShareButton onClick={handleKakaoButton}>
+						<KakaoIcon src={kakaoLogo}></KakaoIcon>
+					</KakaoShareButton>
+				</GridContainer>
+				<BackBtn onClick={()=>(setPopupOpen(false))}>닫기</BackBtn>
+			</FlexContainer>
 		</Popup>
-      );
-  }
+	);
+}
   
 
-  export default SocialShare;
+export default SocialShare;
   
