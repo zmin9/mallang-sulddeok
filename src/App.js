8646d30346dbf865f0018fb3d02ce821d6af1029
zmin9 <o3o0903@gmail.com>
2022-04-06 01:05:50 +0900

    [fix] github page 흰화면 수정

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 const App = () => {
     return(
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route element={<Home/>} path='/'/>
                 <Route element={<Main/>} path='/test'/>
